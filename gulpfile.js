9d332562a3f911e48648714315b7282fef5e0e5e
Tony Edwards <tony@plymouthsoftware.com>
2017-03-11 17:39:30 +0000

    Updates gulp file

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let gulp = require('gulp');
+let runSequence = require('run-sequence');
+
+// Get tasks from gulp-tasks directory
+require('require-dir')('gulp-tasks');
+
+let allTasks = ['scripts', 'copy', 'html', 'images'];
+gulp.task('default', function(cb) {
+  runSequence(
+    'clean',
+    allTasks,
+    'styles',
+    cb);
+});
+
+gulp.task('dev', function() {
+  return runSequence(
+    'clean',
+    allTasks,
+    'styles',
+    'watch',
+    'nodemon');
+});
