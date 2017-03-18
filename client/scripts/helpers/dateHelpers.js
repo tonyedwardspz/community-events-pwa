79a30c97746d564cf5fd9180cdd1cb6c94a9c42b
Tony Edwards <tony@plymouthsoftware.com>
2017-03-18 16:29:50 +0000

    Add date and google analytics helper files

--- client/scripts/helpers/dateHelpers.js
+++ client/scripts/helpers/dateHelpers.js
@@ -1,1 +1,25 @@
 'use strict';
+
+/**
+* Gets a UK formatted date for display to user
+* @param {String} date The full unix date string to convert
+* @return {String} Formatted date string - dd/mm/yyyy
+*/
+let convertDateToLocale = (date) => {
+  try {
+    return new Date(date).toLocaleDateString('en-GB');
+  } catch (e) {
+    console.warn('Error converting date - Trying backup: ' + e);
+    let _date = new Date(date);
+    return `${_date.getDate()}/${_date.getMonth() + 1}/${_date.getFullYear()}`;
+  }
+};
+
+/**
+* Checks to see if the passed date is prior to today
+* @param {String} date The date which needs to be checked
+* @return {Boolean} Whether the date is after today or not
+*/
+let isDateAfterToday = (date) => {
+  return (new Date(date) >= new Date().setHours(0,0,0,0)) ? true : false;
+};
--- client/scripts/helpers/googleAnalyticsHelpers.js
+++ client/scripts/helpers/googleAnalyticsHelpers.js
@@ -1,1 +1,18 @@
 'use strict';
+
+/**
+* Sends a navigation event to Google Analytics if 'ga' is defined. Without this
+* an error is printed to console when an ad block add-on is running.
+* @param {String} view The current view url being displayed
+* @todo Check that this reports correctly in the analytics dashboard
+*/
+let updateGoogleAnalytics = view => {
+  if (ga){
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Navigate',
+      eventAction: view,
+      eventLabel: `Navigate to ${view.split('_')[0]} : ${view.split('_')[1]}`
+    });
+  }
+};
