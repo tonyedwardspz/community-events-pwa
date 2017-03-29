446887b76b9259be19ee5ed821dd57325d2b758a
Tony Edwards <tony@plymouthsoftware.com>
2017-03-29 23:16:44 +0100

    Extracts two helper methods
    Also adds documentation

--- client/scripts/helpers/viewHelpers.js
+++ client/scripts/helpers/viewHelpers.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/**
+* Returns HTML for the provided twitter handle
+* @param {String} handle The event / org twitter handle
+* @return {String} The HTML string for the link
+*/
+let twitterLink = (handle) => {
+  return `<a href="https://twitter.com/
+         ${handle}" target="_blank">
+         https://twitter.com/${handle}</a>`;
+};
+
+/**
+* Returns HTML for the provided link
+* @param {String} url The url to be processed
+* @param {String} title the event / org name for the title attribute
+* @return {String} The HTML string for the link
+*/
+let webLink = (url, title) => {
+  return `<a href="${url}" target="_blank" title="${title} website">${url}</a>`;
+};
--- client/scripts/views/organisationView.js
+++ client/scripts/views/organisationView.js
@@ -8,15 +8,13 @@ class OrganisationView {
 
   /**
   * Returns HTML for the show organisation screen
-  * @param {Organisation} event An organisation object
+  * @param {Organisation} org An organisation object to be displayed
+  * @param {Event.Array} orgEvents The upcoming events
   * @return {String} The HTML string for display
   */
   show(org, orgEvents) {
-    const website = org.website ? `<a href="${org.website}" target="_blank">
-                                   ${org.website}</a>` : '';
-    const twitter = org.twitterHandle ? `<a href="https://twitter.com/
-                                         ${org.twitterHandle}" target="_blank">
-                                         https://twitter.com/${org.twitterHandle}</a>`: '';
+    const website = org.website ? webLink(org.website, org.name) : '';
+    const twitter = org.twitterHandle ? twitterLink(org.twitterHandle): '';
     return `<div class="row">
               <div class="column column-75">
                 <h1>${org.name}</h1>
@@ -35,6 +33,13 @@ class OrganisationView {
             `;
   }
 
+  /**
+  * Returns HTML for the list of upcoming organisation events
+  * @param {Organisation} organisatiom An organisation to be displayed
+  * @param {Event.Array} events The organisations upcoming events
+  * @param {Boolean} [showImage] Should the associated image be displayed?
+  * @return {String} The HTML string for display
+  */
   upcoming(org, events, showImage = true){
     return `${app.eventView.eventList(events, showImage)}`;
   }
