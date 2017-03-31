9fa1eca6b0b9fac5cdaea73568e5f6f815b687ac
Tony Edwards <tony@plymouthsoftware.com>
2017-03-31 02:39:20 +0100

    Removes unused event model code
    I don’t get events from the apps db, so remove the mongoose stuff

--- server/models/event.js
+++ server/models/event.js
@@ -1,37 +1,11 @@
 'use strict';
 
-let mongoose = require('mongoose');
 let BaseModel = require('./base');
 let fetch = require('node-fetch');
 
 class Evnt extends BaseModel {
   constructor() {
     super();
-    this.mongooseModel = mongoose.model('events', this.getMongooseSchema());
-  }
-
-  getMongooseModel() {
-    console.log('getting mongoose model', this.mongooseModel);
-    return this.mongooseModel;
-  }
-
-  getMongooseSchema() {
-    return new mongoose.Schema({
-      id: String,
-      title: String,
-      description: String,
-      organiserID: String,
-      ticketURL: String,
-      date: Date,
-      timeStart: String,
-      timeEnd: String,
-      hashtag: String
-    });
-  }
-
-  getDatabasePromise() {
-    let mongoModel = this.getMongooseModel();
-    return this.getPromise(mongoModel, 'events');
   }
 
   processEventbriteData(events) {
