c354f0f5bcec4d5c85accf736fe408d00037d770
Tony Edwards <tony@plymouthsoftware.com>
2017-04-02 00:05:21 +0100

    Adds tests for the few current string helpers

---
+++ tests/client/helpers/string-helpers-test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let assert = require('assert');
+let fs = require('fs');
+let vm = require('vm');
+let code = fs.readFileSync('./client/scripts/helpers/stringHelpers.js');
+vm.runInThisContext(code);
+
+describe('String Helpers', () => {
+  describe('#random string(length)', () => {
+
+    it('Returns a random string 32 charachters long as default', () => {
+      assert.equal(32, randomString().length);
+    });
+
+    it('Returns a random string of the length passed', () => {
+      assert.equal(10, randomString(10).length);
+      assert.equal(9584, randomString(9584).length);
+    });
+
+  });
+});
