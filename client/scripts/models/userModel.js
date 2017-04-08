129092fcf28c8d64cd85493ed9b4c3976188e49a
Tony Edwards <tony@plymouthsoftware.com>
2017-04-08 15:56:24 +0100

    Adds parsing of user data to client app

--- client/scripts/models/userModel.js
+++ client/scripts/models/userModel.js
@@ -1,7 +1,24 @@
 'use strict';
 
 class User extends BaseModel {
-  constructor() {
+  constructor(userID, twitterID, googleID, email, firstName, lastName,
+      recieveEmail, recievePush, accessToken, refreshToken, isAdmin) {
     super();
+  }
+
+  static processUserData(data) {
+    app.user = new User(
+      data.userID,
+      data.twitterID,
+      data.googleID,
+      data.email,
+      data.firstName,
+      data.lastName,
+      data.recieveEmail,
+      data.recievePush,
+      data.accessToken,
+      data.refreshToken,
+      data.isAdmin
+    );
   }
 }
