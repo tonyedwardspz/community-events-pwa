4e353576c0973221cf50737c68ceece30e5a860b
Tony Edwards <tony@plymouthsoftware.com>
2017-04-13 00:49:37 +0100

    Adds app icons

--- gulp-tasks/copy.js
+++ gulp-tasks/copy.js
@@ -5,14 +5,14 @@ let runSequence = require('run-sequence');
 let del = require('del');
 
 gulp.task('copy:root', function() {
-  del(['./public/*.{json,txt,ico,js}'], {dot: true});
+  del(['./public/*.{json,txt,ico,js,xml}'], {dot: true});
 
-  return gulp.src('./client/*.{json,txt,ico,js,png}')
+  return gulp.src('./client/*.{json,txt,ico,js,png,xml}')
     .pipe(gulp.dest('./public'));
 });
 
 gulp.task('copy:watch', function() {
-  gulp.watch('./public/*.{json,txt,ico,png}', ['copy:root']);
+  gulp.watch('./public/*.{json,txt,ico,png,xml}', ['copy:root']);
 });
 
 gulp.task('copy', function(cb) {
