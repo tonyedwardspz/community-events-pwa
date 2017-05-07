812f4c98ec794a900c83e29638eb0ceca603832e
Tony Edwards <tony@plymouthsoftware.com>
2017-05-07 23:07:20 +0100

    Add documentation

--- server/models/notification.js
+++ server/models/notification.js
@@ -2,6 +2,7 @@
 
 var pushpad = require('pushpad');
 
+/** A class encasulating the pushpad notification interface */
 class Notification {
   constructor() {
     this.pushPadProject = new pushpad.Pushpad({
@@ -10,18 +11,33 @@ class Notification {
     });
   }
 
+  /**
+  * Returns the push pad object
+  * @return {Pushpad} The authenticated pushpad object
+  */
   get pushPadProject() {
     return this._pushPadProject;
   }
 
+  /**
+  * Sets the current ppushpad project
+  */
   set pushPadProject(project){
     this._pushPadProject = project;
   }
 
+  /**
+  * Returns the push pad object
+  * @return {String} The path for the current user, to allow targeting of messages
+  */
   getPushpadURL(userID) {
       return this.pushPadProject.pathFor(userID);
   }
 
+  /**
+  * Returns a ne pushpad object
+  * @return {Pushpad.Notification} The notification object
+  */
   getNewNotification(body, title) {
     return new pushpad.Notification({
       project: this.pushPadProject,
