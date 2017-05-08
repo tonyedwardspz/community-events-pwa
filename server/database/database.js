d69d5eec9208b325826856f0e59a6174665d7670
Tony Edwards <tony@plymouthsoftware.com>
2017-05-08 17:34:53 +0100

    Fix db connection timeout

--- server/database/database.js
+++ server/database/database.js
@@ -8,13 +8,17 @@ class Database {
   }
 
   createConnection(){
-    mongoose.connect(process.env.MONGODB_URI, function (err) {
-      if (err) {
-        console.log ('[Database] Error connecting: ' + err);
-      } else {
-        console.log ('[Database] Succeeded connected');
+    mongoose.connect(process.env.MONGODB_URI, {
+      server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+      replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } },
+      err => {
+        if (err) {
+          console.log ('[Database] Error connecting: ' + err);
+        } else {
+          console.log ('[Database] Succeeded connected');
+        }
       }
-    });
+    );
 
     this.connection = mongoose.connection;
   }
