01cd695020f236ddc52fd7529b426a5fc34abdb9
Tony Edwards <tony@plymouthsoftware.com>
2017-11-18 18:34:22 +0000

    Seperated cache and client data retrival
    This allows responses to be much quicker on the client end. No longer needs to wait for a bazillion calls to eventbrite etc for data

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -9,6 +9,7 @@ let fetch = require('node-fetch');
 let Cache = require('../models/cache');
 let CacheModel = Cache.getMongooseModel();
 let dateHelpers = require('../helpers/dateHelpers');
+let http = require('http');
 
 class DataController extends BaseController {
   constructor() {
@@ -19,88 +20,26 @@ class DataController extends BaseController {
     console.log('[Data Controller] Getting all data');
 
     // Check the cache for recent data
-    let cacheData = {};
     CacheModel.findOne({ id: process.env.CACHE_ID}, function(err, cache) {
       if (cache) {
-        cacheData = cache;
-        // check to see if the cache is fresh
-        if (dateHelpers.lastTwoHours(cache.date)){
-        // if (1 === 2){
-          console.log('[Cache] Is fresh', cache.date);
-          res.send(JSON.stringify(cache.data));
-        } else {
-          console.log('[Cache] Not fresh. Retrieving new data', cache.date);
-          // Get new data
-          let allData = {};
-          let promises = [];
-          promises.push(Organiser.getDatabasePromise('organisers'));
-          promises.push(Subgroup.getDatabasePromise('subgroups'));
+        res.send(JSON.stringify(cache.data));
 
-          // Run all promises and process when all return data or error
-          Promise.all(promises).then(function() {
-            console.log('[Data Controller] All database promises resolved');
-
-            allData.organisations = arguments[0][0].organisers;
-
-            let subgroups = arguments[0][1].subgroups;
-            let urls = Organiser.extractURLS(allData.organisations);
-
-            // Fetch all the events from eventbrite for all organisations
-            Promise.all(urls.map(url =>
-              fetch(url).then(data => data.json())))
-            .then(events => {
-              // Process eventbrite events & store in data structure
-              let data = Evnt.extractEventbriteEvents(events);
-              allData.events = Evnt.processEventbriteData(data);
-              return allData;
-            })
-            // Fetch venues from eventbrite
-            .then(data => Venue.venuesPromise(data.events))
-            // Process eventbrite venues and save venues to data structure
-            .then(venues =>
-              allData.venues = Venue.processEventbriteVenueData(venues)
-            )
-            // Get events from meetup
-            .then(() => Organiser.meetupPromise(allData.organisations))
-            // Process Meetup data, matching subgroups to events & extracting venues
-            .then(events => {
-              let allEvents = [];
-              events.forEach(arr => {
-                arr.map(e => allEvents.push(e));
+        // check to see if the cache is fresh, update if not
+        if (!dateHelpers.lastNHours(cache.date, 6)){
+        // if (1 !== 2){
+          try {
+            http.get({
+                  host: 'community-events-pwa.herokuapp.com',
+                  path: '/updateCache'
+              }, function(response) {
+                  console.log('HTTP response recieved. Cache updated');
               });
-              let processed = Evnt.processMeetupData(allEvents, subgroups);
-              let venues = Venue.processMeetupVenueData(allEvents);
-              processed.map(proc => allData.events.push(proc));
-              venues.map(ven => allData.venues.push(ven));
-              subgroups.map(sub => allData.organisations.push(sub));
-              return;
-            })
-            // Send new data to client
-            .then(() => res.send(JSON.stringify(allData)))
-            // Update the cache
-            .then(() => {
-              cacheData.data = JSON.stringify(allData);
-              cacheData.date = new Date();
-              cacheData.save( err => {
-                if(err) {
-                  console.log(err);
-                } else {
-                  console.log('[Cache] updated');
-                }
-              });
-            })
-            .catch(err => {
-              console.log('Error retrieving events', err);
-              res.status(500).send({ error: 'Error retrieving events' });
-            });
-
-          }, function(err) {
-            console.log('ERROR: ' + err);
-            res.send(500, { error: 'Error getting promise' });
-          });
+          } catch (e){
+            console.log('Could not update the cache');
+          }
         }
       } else {
-        console.log('[Cache] Error', err);
+        console.log('[Data Controller] Error getting data from the cache', err);
       }
     });
   }
--- server/helpers/dateHelpers.js
+++ server/helpers/dateHelpers.js
@@ -1,19 +1,20 @@
 'use strict';
 
 /**
-* Checks that the povided date is within the last 2 hours
+* Checks that the povided date is within the last n hours
 * @param {Date} date The date to be tested
+* @param {Integer} hours The number of hours ago to test
 * @return The generated random string
 */
-exports.lastTwoHours = (date) => {
-  const TWO_HOURS = 60 * 60 * 1000 * 2;
-  const TWO_HOURS_AGO = Date.now() - TWO_HOURS;
+exports.lastNHours = (date, hours) => {
+  const N_HOURS = 60 * 60 * 1000 * hours;
+  const N_HOURS_AGO = Date.now() - N_HOURS;
   let d = new Date(date);
 
-  console.log('[Date Helper]: ' + new Date(TWO_HOURS_AGO));
+  console.log('[Date Helper]: ' + new Date(N_HOURS_AGO));
   console.log('[Date Helper]: ' + new Date(date));
 
-  if (d > TWO_HOURS_AGO) {
+  if (d > N_HOURS_AGO) {
     return true;
   } else {
     return false;
